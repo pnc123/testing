857232eff86d2e8816b78060db5c295ec6732c7d
pnc123 <praveensanpada@gmail.com>
2020-10-27 23:27:36 -0700

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {AddRestoComponent} from './add-resto/add-resto.component'
+import {ListComponent} from './list/list.component'
+import {UpdateRestoComponent} from './update-resto/update-resto.component'
+import {LoginComponent} from './login/login.component'
+import {RegisterComponent} from './register/register.component'
+
+const routes: Routes = [
+{
+	component:AddRestoComponent,
+	path:'add'
+},
+{
+	component:ListComponent,
+	path:'list'
+},
+{
+	component:UpdateRestoComponent,
+	path:'update'
+},
+{
+	component:LoginComponent,
+	path:'login'
+},
+{
+	component:RegisterComponent,
+	path:'register'
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddRestoComponent,
+    UpdateRestoComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
